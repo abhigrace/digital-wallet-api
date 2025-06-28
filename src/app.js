f4b921f00d10c0362c5f24ff7c4573a210d5f8ad
Abhishek Korkora <abhishekkorkora321@gmail.com>
2025-06-29 00:30:46 +0530

    Fix: Corrected router file import

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const apiRoutes = require('./routes/authRoutes');
+const apiRoutes = require('./routes/api');
 
 const app = express();
 
